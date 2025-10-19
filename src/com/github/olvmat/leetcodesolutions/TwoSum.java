dfce85b589953c4e1c50df47427595ed23ef5872
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-10-19 13:11:53 -0300

    feat: two sum solution

--- src/com/github/olvmat/leetcodesolutions/TwoSum.java
+++ src/com/github/olvmat/leetcodesolutions/TwoSum.java
@@ -1,37 +1,31 @@
 package com.github.olvmat.leetcodesolutions;
 
-/*
-Given an array of integers nums and an integer target, return indices of
-the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and
-you may not use the same element twice.
-
-You can return the answer in any order.
-
-Example 1:
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-Example 2:
-
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-
-Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
-
-Constraints:
-
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
-Only one valid answer exists.
-*/
+import java.util.ArrayList;
 
 public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int windowSize = 2;
+        int windowSum = 0;
+
+        ArrayList<Integer> output = new ArrayList<>();
+
+        for (int i = 0; i < windowSize; i++) {
+            output.add(i);
+            windowSum += nums[i];
+        }
+        if (windowSum != target) {
+            for (int i = windowSize; i < nums.length; i++) {
+                int outgoingIndex = i - windowSize;
+                output.removeFirst();
+                output.add(i);
+                windowSum += nums[i] - nums[outgoingIndex];
+                if (windowSum == target) {
+                    return;
+                }
+            }
+        }
+        System.out.println(output);
+    }
 }
